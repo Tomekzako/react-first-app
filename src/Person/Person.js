7b01016f63242118aa9da6d177809f904bde0637
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-07-09 09:56:44 +0200

    Working with props

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Person = () => {
-    return <p>I'm a Person</p>
+const Person = (props) => {
+    return <p>I'm {props.name} and I am {props.age} years old.</p>
 }
 
 
